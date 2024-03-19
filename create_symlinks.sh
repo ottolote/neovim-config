6931fe0d41bd88d0bbaf513f73cb0d30c7db4461
Otto Lote <otto.lote@innofactor.com>
2024-03-19 13:24:47 +0100

    Improve create_symlinks.sh, add remove_symlinks.sh

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-#
-REPO=`dirs +0`
-CONF="~/.config/nvim"
+
+REPO_NVIM_CONFIG=$(dirname "$(readlink --canonicalize "$0")")
+LOCAL_NVIM_CONFIG=$(nvim --headless --cmd 'echo stdpath("config")' --cmd 'quit' 2>&1)
+
+NVIM_FILES="lua init.vim"
 
 # Create config dir if it doesn't exist
-mkdir -p $CONF
-echo "Creating symlinks in $CONF"
-ln -s $REPO/lua $CONF/lua
-ln -s $REPO/init.vim $CONF/init.vim
+mkdir --parents $LOCAL_NVIM_CONFIG
+echo "Creating symlinks from $REPO_NVIM_CONFIG to $LOCAL_NVIM_CONFIG"
 
-ls -alh $CONF
+for file in $NVIM_FILES; do
+    if [ -e $LOCAL_NVIM_CONFIG/$file ]; then
+        echo "Error: File $LOCAL_NVIM_CONFIG/$file already exists, no symlinks were created."
+        exit 1
+    fi
+done
+
+for file in $NVIM_FILES; do
+    ln --symbolic --verbose $REPO_NVIM_CONFIG/$file $LOCAL_NVIM_CONFIG/$file
+done
+
+
---
+++ remove_symlinks.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOCAL_NVIM_CONFIG=$(nvim --headless --cmd 'echo stdpath("config")' --cmd 'quit' 2>&1)
+
+NVIM_FILES="lua init.vim"
+
+# Check if any file is not a symlink and exit early if true
+for file in $NVIM_FILES; do
+    symlink="$LOCAL_NVIM_CONFIG/$file"
+    if [ -e "$symlink" ] && [ ! -L "$symlink" ]; then
+        echo "Error: $symlink exists and is not a symlink."
+        exit 1
+    fi
+done
+
+# Delete symlinks
+for file in $NVIM_FILES; do
+    symlink="$LOCAL_NVIM_CONFIG/$file"
+    if [ -L "$symlink" ]; then
+        echo "Removing symlink $symlink"
+        unlink "$symlink"
+    fi
+done
+
+echo "Neovim configuration symlinks removed"
+
